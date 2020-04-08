6649aad185c999f085b9ab96078f4d22675bb1a8
latryond <latryond@gmail.com>
2020-04-08 22:16:33 +0800

    ref ig feeding

--- pinkland/static/js/main.js
+++ pinkland/static/js/main.js
@@ -1,10 +1,6 @@
 "use strict";
 $(document).ready(function() {
-
-    // Fakes the loading setting a timeout
-    setTimeout(function() {
-        $('body').addClass('loaded');
-    }, 1500);
+    $('body').addClass('loaded');
 
 });
 
--- pinkland/views.py
+++ pinkland/views.py
@@ -6,7 +6,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateView
 from .models import Product, ProductImages, Poster, ProductMaterial, ProductCategory, ProductFunction
-
+from datetime import timedelta
+import requests 
 import logging 
 
 
@@ -16,8 +17,10 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_product'] = Product.objects.all().order_by("-date_create")[:3]
-        context['posters']= Poster.objects.all()
-        # context['parts'] = Product.body_part_choice
+        context['posters'] = Poster.objects.all()
+        context['igposts'] = fetchInstragramPost()
+        for x in context['igposts']['data']:
+            logging.error(x['caption'])
         return context
 
 class ProductList(ListView):
@@ -46,7 +49,8 @@ class ProductDetail(DetailView):
     template_name = "product_detail.html"
     def get_object(self, **kwargs):
         p = Product.objects.get(id=self.kwargs['id'])
-        p.images =  ProductImages.objects.filter(id=self.kwargs['id'])
+        p.images =  ProductImages.objects.filter(p_id=self.kwargs['id'])
+        logging.error(p.images)
         return p
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -70,4 +74,23 @@ class AboutUsPageView(View):
         }
         return render(request, 'aboutus.html', context)
 
+def fetchInstragramPost():
+    IG_APP_ID = 260032918354934
+    ACCESS_TOKEN = refreshInstegramToken()
+    payload = {
+        'fields': 'id,media_type,media_url,caption',
+        'access_token': ACCESS_TOKEN
+        }
+    request = requests.get('https://graph.instagram.com/me/media', params=payload)
+    json = request.json()
+    return json
 
+def refreshInstegramToken():
+    payload = {
+        'grant_type': 'ig_refresh_token',
+        'access_token': 'IGQVJWV20xWERaUU1BWTVkdGJWcE4xcnNEUVB4ZAExITkdILXIza2lXMG5YUG00RzVXb3FmVkpSZAGd6QTlmSHhGbnduSEJnRXNTNXdDS3B4TEhKNDkxdlBrQXUyRTM0SlJITmdFOUVB'
+        }
+    r = requests.get('https://graph.instagram.com/refresh_access_token', params=payload)
+    request_dict = r.json()
+    logging.error(request_dict)
+    return request_dict['access_token']
