bf1935209d975470c477430f89d7a4cadc781b9a
latryond <latryond@gmail.com>
2020-04-19 13:06:44 +0800

    gitignore now working

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/rockpigeon/Documents/GitHub/pinkland/venv/bin/python
+#!/Users/rockpigeon/Documents/GitHub/pinkland/venv/bin/python3.7
 from django.core import management
 
 if __name__ == "__main__":
